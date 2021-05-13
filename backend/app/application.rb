1f900cbf4b958d6d8ff890ececef7cc32334dd76
Michael Corrieri <mlcorrieri@gmail.com>
2021-05-12 21:24:11 -0400

    got rack working Wed 930PM

--- backend/app/application.rb
+++ backend/app/application.rb
@@ -7,6 +7,10 @@ class Application
     if req.path.match(/test/) 
       return [200, { 'Content-Type' => 'application/json' }, [ {:message => "test response!"}.to_json ]]
 
+    elsif req.path == "/states" && req.get?
+      body = State.ordered_list
+      return [200, { 'Content-Type' => 'application/json' }, [ body.to_json ]]
+
     else
       resp.write "Path Not Found"
 
